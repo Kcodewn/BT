af1bc3be96a23391b87d690431ff45dcace10fd8
KnQ4 <kcodewn@gmail.com>
2022-06-11 14:55:01 -0500

    added chart page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('charts/', views.charts, name="charts"),
+
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'accounts/index.html')
+
+def charts(request):
+    return render(request, 'accounts/charts.html')
     
 
