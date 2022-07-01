f253d3ae926b659398aaa54b8a3562e1011b7709
KnQ4 <Kcodewn@gmail.com>
2022-06-30 19:26:39 -0500

    added all pages for BT

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     #SideBar URLs#
     path('', views.home, name="home"),
     path('create_tickets/', views.create_tickets, name="create_tickets"),
+    path('all_tickets/', views.all_tickets, name="all_tickets"),
+    path('my_tickets/', views.my_tickets, name="my_tickets"),
+    path('create_projects/', views.create_projects, name="create_projects"),
+    path('assign_users/', views.assign_users, name="assign_users"),
+    path('remove_users/', views.remove_users, name="remove_users"),
+    path('view_projects/', views.view_projects, name="view_projects"),
     path('assign_roles/', views.assign_roles, name="assign_roles"),
     path('remove_roles/', views.remove_roles, name="remove_roles"),
     
--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,25 @@ def home(request):
 
 def create_tickets(request):
     return render(request, 'accounts/create_tickets.html')
-    
+
+def my_tickets(request):
+    return render(request, 'accounts/my_tickets.html')
+
+def all_tickets(request):
+    return render(request, 'accounts/all_tickets.html')
+
+def create_projects(request):
+    return render(request, 'accounts/create_projects.html')
+
+def assign_users(request):
+    return render(request, 'accounts/assign_users.html')
+
+def remove_users(request):
+    return render(request, 'accounts/remove_users.html')
+
+def view_projects(request):
+    return render(request, 'accounts/view_projects.html')
+
 def assign_roles(request):
     return render(request, 'accounts/assign_roles.html')
 
